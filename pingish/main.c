6ecdd01f088b51083549d6ee2d0c2e11246c7e43
Steve Loughran <stevel@apache.org>
2017-12-30 12:30:24 +0000

    ping structure tracks outcome

--- pingish/main.c
+++ pingish/main.c
@@ -31,6 +31,14 @@
 #define MAX_NO_PACKETS  4
 #define RX_BUF_SIZE 50 * 1024
 
+#define PING_OUTSTANDING 0
+#define PING_SUCCESS 1
+#define PING_FAIL 2
+#define PING_TIMEOUT 3
+#define PING_MISMATCH 4
+#define PING_PACKET_TOO_SMALL 5
+
+
 
 char sendpacket[PACKET_SIZE];
 struct icmp* icmpSendPacket = (struct icmp*)sendpacket;
@@ -50,12 +58,23 @@ pid_t pid;
 
 struct sockaddr_in from;
 
-struct timeval tvrecv;
+typedef struct timeval timestamp;
+timestamp tvrecv;
 
-struct sockaddr_in from;
+/* A ping */
+typedef struct {
+  int status;
+  int seq;
+  int len;
+  struct in_addr dest_addr;
+  timestamp sent;
+  timestamp received;
+  double rtt;
+  int ttl;
+  char *text;
+} ping_t;
 
-struct timeval tvrecv;
-
+timestamp tvrecv;
 
 // forward declarations
 
@@ -65,11 +84,11 @@ unsigned short cal_chksum(unsigned short *addr, int len);
 
 //int build_packet(int packet_id);
 
-void send_packet(void);
+//void send_packet(void);
 
-void recv_packet(void);
+//void recv_packet(void);
 
-int unpack(char *buf, long len);
+//int unpack(char *buf, long len);
 
 
 int quit(int code, const char *msg) {
@@ -84,13 +103,18 @@ int pexit(int code, const char *msg) {
   return code;
 }
 
-void tv_sub(struct timeval *out, struct timeval *in) {
+/*
+ subtract time.
 
-  if ((out->tv_usec -= in->tv_usec) < 0)  {
-    --out->tv_sec;
-    out->tv_usec += 1000000;
+ */
+
+void tv_sub(timestamp *time, timestamp *diff) {
+
+  if ((time->tv_usec -= diff->tv_usec) < 0)  {
+    --time->tv_sec;
+    time->tv_usec += 1000000;
   }
-  out->tv_sec -= in->tv_sec;
+  time->tv_sec -= diff->tv_sec;
 }
 
 
@@ -142,90 +166,117 @@ unsigned short cal_chksum(unsigned short *addr, int len)
  packet_id: id to use in sequence ID.
  */
 
-int build_packet(int packet_id, struct timeval* now)
+int build_packet(int packet_id, timestamp* now)
 {
   int packsize;
-  struct timeval *tval;
+  timestamp *tval;
   icmpSendPacket->icmp_type = ICMP_ECHO;
   icmpSendPacket->icmp_code = 0;
   icmpSendPacket->icmp_cksum = 0;
   icmpSendPacket->icmp_seq = packet_id;
   icmpSendPacket->icmp_id = pid;
   packsize = 8 + datalen;
-  tval = (struct timeval*)icmpSendPacket->icmp_data;
+  tval = (timestamp*)icmpSendPacket->icmp_data;
   *tval = *now;
   icmpSendPacket->icmp_cksum = cal_chksum((unsigned short*)icmpSendPacket, packsize);
   return packsize;
 }
 
-
-void send_packet()
+/*
+ Send a packet
+  ping: ping struct to summarise the request
+  seq: sequence ID
+ */
+int send_packet(ping_t *ping, int seq)
 {
   int packetsize;
-  struct timeval now;
-  while (nsend < MAX_NO_PACKETS) {
+  timestamp now;
+  // ping_t *ping = malloc(sizeof(ping_t));
+  memset(ping, 0, sizeof(ping_t));
+  gettimeofday(&now, NULL);
+  packetsize = build_packet(nsend, &now);
+  if (sendto(sockfd, sendpacket, packetsize, 0,
+             (struct sockaddr*) &dest_addr, sizeof(dest_addr)) < 0) {
+    perror("sendto error");
+    return 0;
+  } else {
+    fprintf(stderr, "sent packet %d\n", nsend);
+    fflush(stderr);
+    ping->sent = now;
+    ping->status = PING_OUTSTANDING;
+    ping->seq = seq;
+    ping->text = NULL;
+    ping->len = packetsize;
     nsend++;
-    gettimeofday(&now, NULL);
-    packetsize = build_packet(nsend, &now);
-    if (sendto(sockfd, sendpacket, packetsize, 0,
-               (struct sockaddr*) &dest_addr, sizeof(dest_addr)) < 0) {
-      perror("sendto error");
-      continue;
-    } else {
-      fprintf(stderr, "sent packet %d\n", nsend);
-      fflush(stderr);
-    }
-    sleep(1);
+    return 1;
   }
 }
 
-void recv_packet()
-{
-  ssize_t n;
-  socklen_t fromlen;
-  extern int errno;
 
-  fromlen = sizeof(from);
-
-  while (nreceived < nsend) {
-    alarm(MAX_WAIT_TIME);
-    if ((n = recvfrom(sockfd,
-                      recvpacket,
-                      sizeof(recvpacket), 0,
-                      (struct sockaddr*) &from, &fromlen)) < 0)
-    {
-      if (errno == EINTR) {
-        continue;
-      }
-      perror("recvfrom error");
-      continue;
-    }
-    gettimeofday(&tvrecv, NULL);
-    if (unpack(recvpacket, n) ==  - 1) continue;
-    nreceived++;
-  }
-
+void flush() {
+  fflush(stdout);
 }
 
+void print_header() {
+  printf("\"outcome\",  \"sequence\", \"length\", \"dest\",\"ttl\", \"rtt\", \"text\"\n");
+  fflush(stdout);
+}
 
-int unpack(char *buf, long len) {
+void print_packet(ping_t *ping) {
+  printf("%d, %d, %d, \"%s\", %d, %0.3f, \"%s\"\n",
+         ping->status,
+         ping->seq,
+         ping->len,
+         (ping->dest_addr.s_addr
+           ? inet_ntoa(ping->dest_addr)
+           : ""),
+         ping->ttl,
+         ping->rtt,
+         ping->text);
+}
+
+ping_t *received_ping(ping_t *ping,
+              int status,
+              int seq,
+              int len,
+              struct in_addr *dest_addr,
+              timestamp *sent,
+              int ttl,
+              double rtt,
+              char *text) {
+  memset(ping, 0, sizeof(ping_t));
+  ping->status = status;
+  ping->seq = seq;
+  if(dest_addr) ping->dest_addr = *dest_addr;
+  if (sent) ping->sent = *sent;
+  ping->ttl = ttl;
+  ping->rtt = rtt;
+  ping->text = text;
+  return ping;
+}
+
+int unpack(char *buf, long len, ping_t *ping, timestamp* now) {
 
   int iphdrlen;
   struct ip *ip;
   struct icmp *icmp;
-  struct timeval *tvsend;
+  timestamp *tvsend;
   double rtt;
   ip = (struct ip*)buf;
   iphdrlen = ip->ip_hl << 2;
   icmp = (struct icmp*)(buf + iphdrlen);
   len -= iphdrlen;
   int rv = -1;
-  if (len < 8)  {
+  if (len < 8) {
     fprintf(stderr, "ICMP packets length less than 8: %ld\n", len);
-    printf("f, %d, \"\", 0, 0, 0, \"packet too small\"\n",
+/*
+ printf("%d, %d, \"\", 0, 0, 0, \"packet too small\"\n",
+           PING_PACKET_TOO_SMALL,
            (int)len);
+ */
+    received_ping(ping, PING_PACKET_TOO_SMALL, 0, 0, NULL, NULL, 0, 0, "Packet too short");
   } else if ((icmp->icmp_type == ICMP_ECHOREPLY) && (icmp->icmp_id == pid))  {
-    tvsend = (struct timeval*)icmp->icmp_data;
+    tvsend = (timestamp*)icmp->icmp_data;
     tv_sub(&tvrecv, tvsend);
     rtt = tvrecv.tv_sec * 1000+tvrecv.tv_usec / 1000;
     fprintf(stderr,
@@ -237,31 +288,80 @@ int unpack(char *buf, long len) {
             rtt);
     // Good value
     // success, length, from, sequence, ttl, rtt
-    printf("s, %d, \"%s\", %u, %d, %0.3f, \"\"\n",
-            (int)len,
-            inet_ntoa(from.sin_addr),
+    /*
+    printf("%d, %d, \"%s\", %u, %d, %0.3f, \"\"\n",
+           PING_SUCCESS,
+           icmp->icmp_id,
+           (int)len,
+           &from.sin_addr,
            icmp->icmp_seq,
            ip->ip_ttl,
            rtt);
-
+     */
+    received_ping(ping,
+                  PING_SUCCESS,
+                  icmp->icmp_seq,
+                  (int)len,
+                  &from.sin_addr,
+                  tvsend,
+                  ip->ip_ttl,
+                  rtt,
+                  "");
     rv = 0;
   } else {
-    printf("f, %d, \"%s\", %u, 0, 0, \"wrong packet type/id\"\n",
+/*
+ printf("%d, %d, \"%s\", %u, 0, 0, \"wrong packet type/id\"\n",
+           PING_MISMATCH,
            (int)len,
            inet_ntoa(from.sin_addr),
            icmp->icmp_seq);
+ */
+    received_ping(ping,
+                  PING_MISMATCH,
+                  icmp->icmp_seq,
+                  (int)len,
+                  &from.sin_addr,
+                  NULL,
+                  0,
+                  0,
+                  "Wrong packet");
   }
-  fflush(stdout);
   return rv;
 }
 
 
+int recv_packet(ping_t *ping) {
+  ssize_t n;
+  socklen_t fromlen;
+  extern int errno;
+
+  fromlen = sizeof(from);
+
+  alarm(MAX_WAIT_TIME);
+  if ((n = recvfrom(sockfd,
+                    recvpacket,
+                    sizeof(recvpacket), 0,
+                    (struct sockaddr*) &from, &fromlen)) < 0)
+  {
+    if (errno == EINTR) {
+      return 0;
+    }
+    perror("recvfrom error");
+    return 0;
+  }
+  gettimeofday(&tvrecv, NULL);
+  unpack(recvpacket, n, ping, &tvrecv);
+  nreceived++;
+  return 1;
+
+}
+
 
 int main(int argc, const char * argv[]) {
-  struct hostent *host;
+//  struct hostent *host;
   struct protoent *protocol;
-  unsigned long inaddr = 0l;
-  int waittime = 10 * 60 * 1000;
+//  unsigned long inaddr = 0l;
+//  int waittime = 10 * 60 * 1000;
   int size = RX_BUF_SIZE;
 
   if (argc != 2) {
@@ -274,7 +374,7 @@ int main(int argc, const char * argv[]) {
     pexit(1, "socket error");
   }
   pid = getpid();
-
+  print_header();
   setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
 
   bzero(&dest_addr, sizeof(dest_addr));
@@ -286,8 +386,14 @@ int main(int argc, const char * argv[]) {
   }
 
   signal(SIGALRM, statistics);
-  send_packet();
-  recv_packet();
+  ping_t ping, rx;
+  if (send_packet(&ping, 1)) {
+    if (recv_packet(&rx)) {
+      print_packet(&rx);
+      flush();
+    }
+  }
+  printf("done\n");
   statistics(SIGALRM);
   return 0;
 
